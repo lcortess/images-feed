7a46d4a73d45c177f7719255652a662d06d0a327
Jose Luis <jose@lcortes.net>
2021-01-08 16:18:28 -0600

    test component

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApiService } from '../api/api.service';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -9,7 +10,8 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
     })
     .compileComponents();
   });
@@ -17,10 +19,27 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+
+    spyOn(component as any, 'getImages');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should start on page 1', () => {
+    expect(component.page).toBe(1);
+  });
+
+  it('Should not be loading', () => {
+    expect(component.isLoading).toBeFalsy();
+  })
+
+  describe('ngOnInit()', () => {
+    it('Should get the first set of images', () => {
+      component.ngOnInit();
+      expect(component['getImages']).toHaveBeenCalled();
+    });
   });
 });
