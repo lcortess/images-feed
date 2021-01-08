35a251263e4ddf2626f506f746501e7db2e994ad
Jose Luis <jose@lcortes.net>
2021-01-08 15:08:40 -0600

    infinite scroll for images

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ApiService } from '../api/api.service';
 import { Image } from '../api/services/images.service';
 
@@ -10,9 +10,47 @@ import { Image } from '../api/services/images.service';
 export class HomeComponent implements OnInit {
   public page = 1;
   public images: Array<Image> = [];
+  public isLoading: boolean = false;
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.getImages();
+  }
+
+  /**
+   * Listen for the scroll event to load more images while scrolling
+   * 
+   * @param $event 
+   */
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event: any) {
+    const scrollHeight: number = document.documentElement.scrollHeight;
+    const scrollPage: number = window.innerHeight + $event.target['scrollingElement'].scrollTop;
+    
+    // fire if the scroll position is 200px above the bottom load more
+    if( scrollPage > scrollHeight - 200 ) {
+      this.getImages()
+    }
+
+  }
+
+  /**
+   * Fetch images from the API
+   */
+  private getImages(): void {
+    // If is already getting images exit the function
+    if (this.isLoading) {
+      return;
+    }
+
+    this.isLoading = true;
+    this.api.images.list(this.page++).subscribe((images) => {
+      this.images.push(...images);
+      this.isLoading = false;
+    }, error => {
+      this.isLoading = false;
+      alert('Error fetching images');
+    });
   }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LazyImageDirective } from './directives/lazy-image.directive';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LazyImageDirective],
   imports: [
     CommonModule,
     HomeRoutingModule
